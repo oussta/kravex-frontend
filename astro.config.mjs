dc575a4f39b8e599b42f81887f32d676972a3620
oussta <ousstatada@gmail.com>
2026-02-26 20:55:21 +0100

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://kravex-frontend.vercel.app',
   output: 'server',
   adapter: vercel(),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
